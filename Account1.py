3ea5129853307e9da15d7816fc6d15dcfea77784
Parvathareddy Nagavarun Kumar <119427732+Nagavarunkumarparvathareddy@users.noreply.github.com>
2024-08-28 22:16:50 +0530

    Add files via upload

---
+++ Account1.py
@@ -0,0 +1,6 @@
+class Account:
+    def __init__(self):
+        self.__accno = 10102542
+        self.accname = 'varun'
+        self.__pinno = 5564
+        self.branch = 'HYD'
---
+++ Account2.py
@@ -0,0 +1,6 @@
+from Account1 import Account
+ac = Account()
+print(ac.accno)
+print(ac.pinno)
+print(ac.branch)
+print(ac.accname)
